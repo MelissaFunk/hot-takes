fae0224455a8b6caad3ee8e592283a6fd3962429
Melissa Funk <melissafunk85@gmail.com>
2022-09-01 17:19:19 -0700

    added more categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 melissa = User.create(username: "melissafunk", password: "hello")
 
 c1 = Category.create(category: "Fast Food Chains")
-c2 = Category.create(category:"Funniest Celebrities")
+c2 = Category.create(category:"Funniest Celebrities/Comedians")
 c3 = Category.create(category:"Christmas Movies")
 
-l1 = List.create(num1: "Shake Shack", num2: "In-n-Out", num3: "Del Taco", num4: "Popeyes", num5: "Togos", likes: 0, user_id: 1, category_id: 1, likes: 0)
+l1 = List.create(num1: "Shake Shack", num2: "In-n-Out", num3: "Del Taco", num4: "Popeyes", num5: "Togos", likes: 0, user_id: 1, category_id: 1)
